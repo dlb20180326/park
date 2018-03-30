9cfd1f7039a71df59535a43561323821e99977b7
Realm <realm.x@hotmail.com>
2018-03-31 04:04:33 +0800

    new files

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import store from '../store';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+const router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: () => import('@/views/main'),
+            meta: { requiresAuth: true }
+        },
+        {
+            path: '/login',
+            component: () => import('@/views/login')
+        },
+        {
+            path: '/about',
+            component: () => import('@/views/about'),
+            meta: { requiresAuth: true }
+        }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.state.user.id) {
+            next({
+                path: '/login'
+                // query: { redirect: to.fullPath }
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
     }
-  ]
-})
+});
+
+export default router;
