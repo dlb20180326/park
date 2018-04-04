d9d597b89bc761ae7b0f530399d062ba6c330f07
Realm <realm.x@hotmail.com>
2018-04-04 16:25:08 +0800

    根据不同角色显示不同tab栏

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,23 +1,45 @@
 <template>
     <div>
         <router-view/>
-		<tabbar slot="bottom">
-        	<tabbar-item>
-        		<img slot="icon" src="@/assets/images/gray-home.png">
-        		<img slot="icon-active" src="@/assets/images/iconw-home.png">
-        		<span slot="label">首页</span>
-      		</tabbar-item>
-      		<tabbar-item active-class="reds">
-        		<img slot="icon" src="@/assets/images/gray-item.png">
-        		<img slot="icon-active" src="@/assets/images/iconw-integral.png">
-        		<span slot="label">积分详情</span>
-      		</tabbar-item>
-      		<tabbar-item>
-        		<img slot="icon" src="@/assets/images/gray-active.png">
-        		<img slot="icon-active" src="@/assets/images/iconw-activity.png">
-        		<span slot="label">活动详情</span>
-      		</tabbar-item>
-    	</tabbar>
+        <tabbar slot="bottom" v-if="roleid==3">
+            <tabbar-item>
+                <img slot="icon" src="@/assets/images/gray-home.png">
+                <img slot="icon-active" src="@/assets/images/iconw-home.png">
+                <span slot="label">首页</span>
+            </tabbar-item>
+            <tabbar-item active-class="reds">
+                <img slot="icon" src="@/assets/images/gray-item.png">
+                <img slot="icon-active" src="@/assets/images/iconw-integral.png">
+                <span slot="label">积分详情</span>
+            </tabbar-item>
+            <tabbar-item>
+                <img slot="icon" src="@/assets/images/gray-active.png">
+                <img slot="icon-active" src="@/assets/images/iconw-activity.png">
+                <span slot="label">活动详情</span>
+            </tabbar-item>
+        </tabbar>
+        <tabbar slot="bottom" v-if="roleid==4">
+            <tabbar-item>
+                <img slot="icon" src="@/assets/images/gray-home.png">
+                <img slot="icon-active" src="@/assets/images/iconw-home.png">
+                <span slot="label">首页</span>
+            </tabbar-item>
+            <tabbar-item active-class="reds">
+                <img slot="icon" src="@/assets/images/gray-info.png">
+                <img slot="icon-active" src="@/assets/images/iconw-partyMembe.png">
+                <span slot="label">党员信息</span>
+            </tabbar-item>
+            <tabbar-item active-class="reds">
+                <img slot="icon" src="@/assets/images/gray-item.png">
+                <img slot="icon-active" src="@/assets/images/iconw-integral.png">
+                <span slot="label">积分详情</span>
+            </tabbar-item>
+            <tabbar-item>
+                <img slot="icon" src="@/assets/images/gray-active.png">
+                <img slot="icon-active" src="@/assets/images/iconw-activity.png">
+                <span slot="label">活动详情</span>
+            </tabbar-item>
+        </tabbar>
     </div>
 </template>
 
@@ -25,6 +47,11 @@
 import { Tabbar, TabbarItem } from 'vux';
 
 export default {
+    data() {
+        return {
+            roleid: this.$store.getters.user.roleid
+        };
+    },
     components: {
         Tabbar,
         TabbarItem
@@ -33,10 +60,31 @@ export default {
 </script>
 
 <style scoped>
-.vux-flexbox{width:89.4%;height:.8rem;margin:.13rem auto .32rem auto;}
-.flex-demo{width:98%;height:.8rem;background:rgba(246,246,246,1);border-radius: 2px;}
-.vux-flexbox-item:nth-child(2){margin-right:-1%;}
-#echartShow{width:76%;height:2.1rem;margin:0 auto;}
-.weui-tabbar{background-color:#FFFFFF;width:100%;position:fixed;}
-.weui-tabbar__item{padding:2px 0 0;}
+.vux-flexbox {
+    width: 89.4%;
+    height: 0.8rem;
+    margin: 0.13rem auto 0.32rem auto;
+}
+.flex-demo {
+    width: 98%;
+    height: 0.8rem;
+    background: rgba(246, 246, 246, 1);
+    border-radius: 2px;
+}
+.vux-flexbox-item:nth-child(2) {
+    margin-right: -1%;
+}
+#echartShow {
+    width: 76%;
+    height: 2.1rem;
+    margin: 0 auto;
+}
+.weui-tabbar {
+    background-color: #ffffff;
+    width: 100%;
+    position: fixed;
+}
+.weui-tabbar__item {
+    padding: 2px 0 0;
+}
 </style>
