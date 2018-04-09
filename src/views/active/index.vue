f22de26d54db0636e09062fba37773349b16ad68
Realm <realm.x@hotmail.com>
2018-04-09 22:30:31 +0800

    调整样式

--- src/views/active/index.vue
+++ src/views/active/index.vue
@@ -125,14 +125,18 @@ export default {
             wx.ready(() => {
                 // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
                 console.log('wx config success');
+                wx.checkJsApi({
+                    jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+                    success: res => {
+                        // 以键值对的形式返回，可用的api值true，不可用为false
+                        // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+                        console.log('checkJsApi success');
+                    }
+                });
             });
-            wx.checkJsApi({
-                jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-                success: res => {
-                    // 以键值对的形式返回，可用的api值true，不可用为false
-                    // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-                    console.log('checkJsApi success');
-                }
+            wx.error(res => {
+                console.log('微信JS SDK配置错误！');
+                // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
             });
         });
     },
@@ -220,7 +224,8 @@ export default {
             align-items: center;
             justify-content: center;
             img {
-                min-width: 100%;
+                max-width: 100%;
+                max-height: 100%;
             }
             &.btn-plus {
                 background-image: url(../../assets/images/add_bg.png);
