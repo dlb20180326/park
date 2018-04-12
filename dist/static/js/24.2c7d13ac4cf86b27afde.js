8a92ee557e1c62b7f9b9a0e335cac37c927a341c
Realm <realm.x@hotmail.com>
2018-04-12 13:57:51 +0800

    完善微信上传

--- dist/static/js/24.2c7d13ac4cf86b27afde.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([24],{aqQa:function(e,t){},qsx3:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=a("XyMi");var i=function(e){a("aqQa")},c=Object(n.a)({},function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n    积分评定\n")])},[],!1,i,"data-v-5f3f2a35",null);t.default=c.exports}});
-//# sourceMappingURL=24.2c7d13ac4cf86b27afde.js.map
