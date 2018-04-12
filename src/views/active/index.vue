0681c9dd3a3250831959bc162db098ff7f6407f4
Realm <realm.x@hotmail.com>
2018-04-12 16:29:36 +0800

    优化代码

--- src/views/active/index.vue
+++ src/views/active/index.vue
@@ -147,12 +147,16 @@ export default {
                     new Promise(resolve => {
                         let serverIds = [];
                         let toUpload = localId =>
-                            this.uploadImage(localId).then(serverId => {
-                                serverIds.push(serverId);
-                                if (localIds.length) {
-                                    toUpload(localIds.shift());
-                                } else {
-                                    resolve(serverIds);
+                            wx.uploadImage({
+                                localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
+                                isShowProgressTips: 1, // 默认为1，显示进度提示
+                                success: res => {
+                                    serverIds.push(res.serverId);
+                                    if (localIds.length) {
+                                        toUpload(localIds.shift());
+                                    } else {
+                                        resolve(serverIds);
+                                    }
                                 }
                             });
                         if (localIds.length) {
@@ -179,15 +183,7 @@ export default {
                     });
                 }
             });
-        },
-        uploadImage: localId =>
-            new Promise(resolve =>
-                wx.uploadImage({
-                    localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
-                    isShowProgressTips: 1, // 默认为1，显示进度提示
-                    success: res => resolve(res.serverId)
-                })
-            )
+        }
     }
 };
 </script>
