f8b5b75a9e0ae8ebc1fd206396668b9df74d6ee9
Realm <realm.x@hotmail.com>
2018-04-12 10:29:32 +0800

    测试图片上传

--- src/views/active/index.vue
+++ src/views/active/index.vue
@@ -143,6 +143,7 @@ export default {
                 success: res => {
                     // const localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                     (res.localIds || []).map(localId => {
+                        this.localIds.push(localId);
                         wx.uploadImage({
                             localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
                             isShowProgressTips: 1, // 默认为1，显示进度提示
@@ -161,12 +162,12 @@ export default {
                                     });
                             }
                         });
-                        wx.getLocalImgData({
-                            localId: localId, // 图片的localID
-                            success: res => {
-                                var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
-                            }
-                        });
+                        // wx.getLocalImgData({
+                        //     localId: localId, // 图片的localID
+                        //     success: res => {
+                        //         var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
+                        //     }
+                        // });
                     });
                 }
             });
