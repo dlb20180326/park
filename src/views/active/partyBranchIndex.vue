2718935129d3e340dedcd6cdc1bc2dd824491808
sqin <379785771@qq.com>
2018-04-16 20:02:29 +0800

    111111111
    1111111111

--- src/views/active/partyBranchIndex.vue
+++ src/views/active/partyBranchIndex.vue
@@ -21,7 +21,7 @@
                     </router-link>
                     <flexbox class="images-preview" :gutter="0" wrap="wrap">
                         <flexbox-item :span="1/3" v-for="(it,second) in item.pictureList" :key="second">
-                            <div><img :src="it.src" class="previewer-demo-img widthSet" @click="atShow(second,index)"></div>
+                            <div><img :src="it.src" class="previewer-demo-img widthSet"  width="100" @click="atShow(second,index)"></div>
                         </flexbox-item>
                         <div v-transfer-dom>
 				      		<previewer :list="item.pictureList" ref="previewer" :options="options" @on-index-change="logIndexChange">
@@ -137,7 +137,6 @@ export default {
     		})
     	},
         chooseImage(its){
-            alert("111111111111")
             console.log(its)
             var count = its.pictureList.length;
             var it = its;
