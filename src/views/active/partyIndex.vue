6dc528fb001c0da217d337ca949caa96099afde5
李小乐er <1974931363@qq.com>
2018-04-16 13:11:16 +0800

    图片适配

--- src/views/active/partyIndex.vue
+++ src/views/active/partyIndex.vue
@@ -21,7 +21,7 @@
                     </router-link>
                     <flexbox class="images-preview" :gutter="0" wrap="wrap">
                         <flexbox-item :span="1/3" v-for="(it,second) in item.pictureList" :key="second">
-                            <div><img :src="it.src" class="previewer-demo-img" @click="atShow(second,index)"></div>
+                            <div><img :src="it.src" class="previewer-demo-img widthSet" @click="atShow(second,index)"></div>
                         </flexbox-item>
                         <div v-transfer-dom>
 				      		<previewer :list="item.pictureList" ref="previewer" :options="options" @on-index-change="logIndexChange">
@@ -192,6 +192,9 @@ export default {
     display: flex;
     flex-direction: column;
     background-color: #efefef;
+}
+.widthSet{
+	width:100%;
 }
 .box {
     flex: 1;
