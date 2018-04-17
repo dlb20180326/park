125b82ec97fca536a814cdbaab4cadd6b423ffeb
KittySnow <158086780@qq.com>
2018-04-17 15:32:34 +0800

    33333333

--- config/index.js
+++ config/index.js
@@ -10,16 +10,16 @@ module.exports = {
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
         proxyTable: {
-           '/dangjian/': {
+           /*'/dangjian/': {
                 changeOrigin: true,
-                target: 'http://www.dlbdata.cn:8018',
+                target: 'http://www.dlbdata.cn:8018/',
                 // target:'192.168.1.173:8080/',
                 pathRewrite: { '^/dangjian/': '/dangjian/' }
-            }
-            /*'/': {
-                changeOrigin: true,
-                target:'http://127.0.0.1/6666'
             }*/
+            '/': {
+                changeOrigin: true,
+                target:'http://www.dlbdata.cn:8018/dangjian'
+            }
         },
 
         // Various Dev Server settings
