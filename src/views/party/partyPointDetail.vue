31c131f82bd7ca5c77ee1663daf6dca211fca847
zhangxiaoshun <15010969174@163.com>
2018-04-28 16:25:06 +0800

    Revert "更改"
    This reverts commit a90f824e5629b4fb920885f0963c667dc877ab86.

--- src/views/party/partyPointDetail.vue
+++ src/views/party/partyPointDetail.vue
@@ -107,7 +107,6 @@ export default {
                     return this.getFullYear() +'.'+months+'.'+dates+' '+hours+':'+minutes
                 }
                 return new Date(value).toLocaleString();}
-            
         },
         Upper: function (value) {
              return value> 0 ? '+' + value : value;
