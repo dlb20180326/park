86a70d1b0a54baf1f3791c6229fc0b2d33b9cafb
zhangxiaoshun <15010969174@163.com>
2018-04-28 16:10:56 +0800

    没有信息时的提示

--- src/views/points/political.vue
+++ src/views/points/political.vue
@@ -15,7 +15,12 @@
                         <flexbox-item>时间</flexbox-item>
                         <flexbox-item>评分状态</flexbox-item>
                     </flexbox>
-                    <flexbox  style="text-align: center" v-if="list.length === 0">暂无支部人员提交信息</flexbox>
+                    <flexbox  style="text-align: center" v-if="list.length === 0">
+                		<div>
+                			
+                		</div>
+                    	<p style="color: #CCCCCC;margin: auto;">暂无支部人员提交信息</p>
+                    </flexbox>
                     <flexbox  :gutter="0"  v-for="(con,index) in list" :key="index">
                         <flexbox-item>{{index+1}}</flexbox-item>
                         <flexbox-item>{{con.partyname}}</flexbox-item>
