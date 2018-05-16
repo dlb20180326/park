716b4c5d6577957c6b73886faf6a5cd996284add
肖伟 <james112496@163.com>
2018-05-16 10:34:15 +0800

    负责人箭头显示

--- src/views/loginManage.vue
+++ src/views/loginManage.vue
@@ -27,7 +27,7 @@
                     </x-button>
                     <popup-picker :show.sync="PickerVisible" :columns="1" :show-cell="false" :data="pickerList" v-model="pickerValue" confirm-text="确认" @on-change="roleChange"></popup-picker>
                     <!-- <input type="text" v-model="roleName" style="color:#ccc;" placeholder="请选择管理角色" @click="PickerVisible=true" readonly/> -->
-                    <!-- <span class="icon-phone2" style="float:right" ></span> -->
+                    <span class="icon-phone2" style="float:right" ></span>
                 </div>
                 <div class="input-all clearfix">
                     <span class="icon-phone"></span>
